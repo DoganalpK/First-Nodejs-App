351808df897da8eae179ce2165ed4ea24f5fb04a
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-18 15:49:37 +0300

    dinamik list

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ app.set("view engine", "ejs");
 
 const data = [
     { id: 1, name: "iphone 11", price: 30000, isActive: true },
-    { id: 2, name: "iphone 12", price: 35000, isActive: true},
-    { id: 3, name: "iphone 13", price: 40000, isActive: true},
+    { id: 2, name: "iphone 12", price: 35000, isActive: true },
+    { id: 3, name: "iphone 13", price: 40000, isActive: false },
 ]
 
 
 //routes
 app.use("/products/:id", (req, res) => {
-    res.render("product-detail");
+    const _product = data.find(o => o.id == req.params.id);
+    res.render("product-detail", _product);
 });
 
 app.use("/products", (req, res) => {
