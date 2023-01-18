eaa4e8829a2b2d109a7a17cbc0339549be749359
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-18 15:11:30 +0300

    first_nodejs

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 //Server oluştur
 var http = require("http");
 
+//filesystem
+var fs = require("fs");
+
 var server = http.createServer((req, res) => {
 
-    if(req.url == "/"){
-        res.write("<h1>Index</h1>");
+    if (req.url == "/") {
+        fs.readFile("index.html", (err, html) => {
+            res.write(html);
+            res.end();
+        });
     }
-    else if(req.url == "/urunler"){
-        res.write("<h1>Urunler</h1>");
+    else if (req.url == "/urunler") {
+        fs.readFile("urunler.html", (err, html) => {
+            res.write(html);
+            res.end();
+        })
     }
-    else{
-        res.write("sayfa bulunamadı");
-    }
-    res.end();
+    else {
+        fs.readFile("404.html", (err, html) => {
+            res.write(html);
+            res.end();
+        })
+    }    
 });
 
 //Port aç
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log("node.js server at port 3000");
 });
