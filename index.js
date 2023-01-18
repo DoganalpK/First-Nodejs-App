a2fcf2ca5b6832c6c64136be3a24cdf776eac2cc
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-18 15:44:14 +0300

    product list

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.set("view engine", "ejs");
 
 
 const data = [
-    { id: 1, name: "iphone 11", price: 30000 },
-    { id: 2, name: "iphone 12", price: 35000 },
-    { id: 3, name: "iphone 13", price: 40000 },
+    { id: 1, name: "iphone 11", price: 30000, isActive: true },
+    { id: 2, name: "iphone 12", price: 35000, isActive: true},
+    { id: 3, name: "iphone 13", price: 40000, isActive: true},
 ]
 
 
