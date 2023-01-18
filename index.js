e73192f07f283ee1e1d51527a1608e180f822845
Doganalp KARATAS <doganalp.karatas@avatek.com.tr>
2023-01-18 16:44:24 +0300

    static files

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 
 const data = [
-    { id: 1, name: "iphone 11", price: 30000, isActive: true },
-    { id: 2, name: "iphone 12", price: 35000, isActive: true },
-    { id: 3, name: "iphone 13", price: 40000, isActive: false },
+    { id: 1, name: "iphone 11", price: 30000, isActive: true, imgPath: "image1.jpg" },
+    { id: 2, name: "iphone 12", price: 35000, isActive: true, imgPath: "image1.jpg" },
+    { id: 3, name: "iphone 13", price: 40000, isActive: false, imgPath: "image1.jpg" },
 ]
 
 
